1379a2af6c45822d0dea131a2b6f946838b203d1
aaa <jeongseok95@naver.com>
2020-02-07 18:01:09 +0900

    20_02_07

--- SWEA/8_1.py
+++ SWEA/8_1.py
@@ -1,3 +1,14 @@
-S = input()
-for i in range(len(S)):
-    S[i]
+def Palindrome(S):
+
+    for i in range(len(S)):
+
+        if s[i] != s[len(S)-1-i]:
+            return False
+        return True
+
+s = input()
+print(s)
+if Palindrome(s):
+    print('입력하신 단어는 회문(Palindrome)입니다.')
+else:
+    print('입력하신 단어는 회문(Palindrome)이 아닙니다.')
