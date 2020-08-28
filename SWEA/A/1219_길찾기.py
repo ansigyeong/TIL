b61cd2e61b167dc32c1e197e620ffde518d1fef0
안시경 <jeongseok95@naver.com>
2020-08-28 13:35:07 +0900

    TIL

--- SWEA/A/1219_길찾기.py
+++ SWEA/A/1219_길찾기.py
@@ -3,7 +3,7 @@ def generator():
     tc, N = map(int, input().strip().split())
 
     start = None
-    for v in map(int, input().strip(). split()):
+    for v in map(int, input().strip().split()):
         if start is None:
             start = v
         else:
@@ -15,8 +15,26 @@ def generator():
     yield tc, table
 
 
-def solve():
-    pass
+def solve(table):
+    visited = [False] * 100
+    next_pos = table['0'] # queue
+    while next_pos:
+        pos = next_pos.pop()
+
+        # Check visited city
+        if visited[int(pos)]:
+            continue
+        else:
+            # If not visited, check linked cities
+            add_next_pos = []
+            if pos in table:
+                add_next_pos = table[pos]
+            # Check if '99' city is in next city's List
+            if '99' in add_next_pos:
+                return 1
+            next_pos.extend(add_next_pos)
+    return 0
+
 
 for _ in range(10):
     for tc, table in generator():
--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
-def gen():
+def generator():
+    tc, N = map(int, input().strip().split())
     table = dict()
-    t, e = map(int, input().strip().split())
 
     start = None
     for v in map(int, input().strip().split()):
@@ -10,34 +10,15 @@ def gen():
             if str(start) in table:
                 table[str(start)].append(str(v))
             else:
-                table[str(start)] = [str(v)]
+                table[str(start)] = str(v)
             start = None
-    yield t, table
+    yield tc, table
 
 
 def solve(table):
-    visited = [False] * 100
-    next_pos = table['0']
-    while next_pos:
-        pos = next_pos.pop()
-
-        # Check visited city
-        if visited[int(pos)]:
-            continue
-        else:
-            # If not visited, check linked cities
-            add_next_pos = []
-            if pos in table:
-                add_next_pos = table[pos]
-            # Check if '99' city is in next city's List
-            if '99' in add_next_pos:
-                return 1
-
-            next_pos.extend(add_next_pos)
-    return 0
+    
 
 
-if __name__ == "__main__":
-    for i in range(10):
-        for t, table in gen():
-            print("#{0} {1}".format(t, solve(table)))
+for _ in range(10):
+    for tc, table in generator():
+        print('#{} {}'.format(tc, solve(table)))
