ee909a47c50115c71e81191d862132ee3bf2ebd2
An Sigyeong <jeongseok95@naver.com>
2021-01-02 23:21:50 +0900

    TIL

---
+++ 백준/DFS/2178_미로탐색.py
@@ -0,0 +1,15 @@
+def dfs(v, visit):
+    visit += [v]
+
+
+n, m = map(int, input().split()) # 세로, 가로
+
+matrix = [list(map(int, input())) for _ in range(n)]
+
+# 위, 아래, 좌, 우
+dn = [-1, 1, 0, 0]
+dm = [0, 0, -1, 1]
+
+startN, startM = 0, 0
+
+dfs(startN, startM, [])
